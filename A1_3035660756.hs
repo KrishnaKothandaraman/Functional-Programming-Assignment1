49a4346f40677bfb64a32ddf5f6768906cbcbe9a
krishna712001 <63452545+krishna712001@users.noreply.github.com>
2021-09-24 11:36:29 +0800

    ismember completed

--- A1_3035660756.hs
+++ A1_3035660756.hs
@@ -3,6 +3,6 @@ fibonacci 0 = 0
 fibonacci 1 = 1
 fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
 
-isMember :: a -> [b] -> Bool
-isMember _ [] = False
-isMember a (x:xs) = a == x || isMember a xs
+ismember :: [Char] -> [[Char]] -> Bool
+ismember _ [] = False
+ismember a (x:xs) = a == x || ismember a xs
