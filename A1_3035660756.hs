78f1f2936668f8ef4f1a31aaf8b2a8d2a409ea64
krishna712001 <63452545+krishna712001@users.noreply.github.com>
2021-09-24 15:22:41 +0800

    Wave completed

--- A1_3035660756.hs
+++ A1_3035660756.hs
@@ -6,3 +6,29 @@ fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
 ismember :: [Char] -> [[Char]] -> Bool
 ismember _ [] = False
 ismember a (x:xs) = a == x || ismember a xs
+
+-- isStepped :: [Int] -> Bool
+-- isStepped [] = True
+-- isStepped x | length x >= 3 && x !! 0 == x !! 1 == x !! 2 = True && isStepped (drop 3 x)
+--             | otherwise = False
+
+sort :: [Int] -> [Int]
+sort [] = []
+sort(x:xs) = sort ys ++ [x] ++ sort zs
+    where
+        ys = [a | a <- xs, a <= x]
+        zs = [b | b <- xs, b > x]
+
+getmiddle :: [Int] -> [Int]
+getmiddle [] = []
+getmiddle (x:xs) | length xs >= 1 = tail (reverse xs)
+                 | otherwise = [x]
+
+wave' :: [Int] -> [Int]
+wave' x | length x == 1 = x
+        | otherwise = head (reverse x) : head x : wave (getmiddle x)
+
+
+wave :: [Int] -> [Int]
+wave [] = []
+wave x = wave' (sort x)
